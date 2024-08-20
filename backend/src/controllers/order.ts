a8bed93d4b945ad3f2a9813e702920b8edba32de
Artem Aisin <artem.aisin@yandex.ru>
2024-08-21 00:33:08 +0300

    fix: some changes

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import { Request, Response, NextFunction } from 'express';
 import { faker } from '@faker-js/faker';
 import { Error as MongooseError } from 'mongoose';
@@ -10,15 +11,12 @@ const placeOrder = async (req: Request, res: Response, next: NextFunction) => {
   const { total, items } : IOrder = req.body;
 
   try {
-    if (typeof total !== 'number' || total === 0) {
-      return next(new BadRequestError('Неправильное значение поля total'));
-    }
-
-    if (!Array.isArray(items) || items.length === 0) {
-      return next(new BadRequestError('Неправильное значение поля items'));
-    }
-
     const products = await Product.find({ _id: { $in: items } });
+
+    if (products.find((item) => item.price == null)) {
+      return next(new BadRequestError('Передан товар, который не продается'));
+    }
+
     const productSum = products.reduce((sum, item) => {
       if (item.price !== null) {
         return sum + Number(item.price);
@@ -26,12 +24,12 @@ const placeOrder = async (req: Request, res: Response, next: NextFunction) => {
       return sum;
     }, 0);
 
-    if (items.length !== products.length) {
-      return next(new BadRequestError('Передан неправильный id'));
-    }
-
     if (total !== productSum) {
       return next(new BadRequestError('Неправильная сумма товаров'));
+    }
+
+    if (items.length !== products.length) {
+      return next(new BadRequestError('Передан неправильный id'));
     }
 
     return res.status(200).send({
